0259ae306389f201493cd0fc5791aa97f0b1ed42
mateuszkitlas <mateusz.kitlas@gmail.com>
2021-04-27 20:15:52 +0200

    windows devices and errors

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,19 @@ export interface Options {
   dpi: number;
   format: 'png' | 'jpg' | 'tiff' | 'gif' | 'bmp';
   timeout?: number;
+  device?: string;
 }
 
+export module Errors {
+  const e = (msg: string, code: number) => {
+    const err = new Error(msg);
+    (err as any)['code'] = code;
+    return err;
+  }
+  export const connect = e("connect error", -1);
+  export const timeout = e("timeout error", -2);
+  export const busy = e("the device is busy", -3);
+}
 export const defaultTimeout = 60 * 1000;
 
 const toBuffer = (proc: ChildProcessByStdio<null, Readable, Readable>, timeout?: number) =>
@@ -18,7 +29,7 @@ const toBuffer = (proc: ChildProcessByStdio<null, Readable, Readable>, timeout?:
     const handler = (timeout || defaultTimeout)
       ? setTimeout(() => {
         proc.kill();
-        reject("timeout");
+        reject(Errors.timeout);
       }, (timeout || defaultTimeout))
       : undefined;
     proc.on('close', (code) => {
@@ -29,38 +40,51 @@ const toBuffer = (proc: ChildProcessByStdio<null, Readable, Readable>, timeout?:
         clearTimeout(handler);
       }
     });
-    
   });
 
 const run = (cmd: string, ...args: string[]) => spawn(cmd, args, { stdio: ['ignore', 'pipe', 'pipe'] })
 
-const windowsScan = (o: Options) => {
+const powershell = (data: string) => run("powershell.exe", '-NoProfile', '-Command', `& {${data}}`)
+
+const windowsScan = async (o: Options) => {
   const fid = (x: string) => `{B96B3C${x}-0728-11D3-9D7B-0000F81EF32E}`
   const formatID = fid({ bmp: 'AB', png: 'AF', gif: 'B0', jpg: 'AE', tiff: 'B1' }[o.format]);
-  const ps = `
-    $ErrorActionPreference = "Stop"
-    $deviceManager = new-object -ComObject WIA.DeviceManager
-    $device = $deviceManager.DeviceInfos.Item(1).Connect()
-    foreach ($item in $device.Items) {
-      foreach($prop in $item.Properties){
-        if(($prop.PropertyID -eq 6147) -or ($prop.PropertyID -eq 6148)){ $prop.Value = "${o.dpi}" }
-      }
+  try {
+    return await toBuffer(powershell(`
+        $ErrorActionPreference = "Stop"
+        $deviceManager = new-object -ComObject WIA.DeviceManager
+        ${o.device
+          ? `$device = $deviceManager.DeviceInfos | where DeviceID -eq "${o.device}"`
+          : "$device = $deviceManager.DeviceInfos.Item(1)"
+        }
+        $deviceConnected = $device.Connect()
+        foreach ($item in $deviceConnected.Items) {
+          foreach($prop in $item.Properties){
+            if(($prop.PropertyID -eq 6147) -or ($prop.PropertyID -eq 6148)){ $prop.Value = "${o.dpi}" }
+          }
+        }
+        $imageProcess = new-object -ComObject WIA.ImageProcess
+        foreach ($item in $deviceConnected.Items) {
+          $image = $item.Transfer()
+        }
+        $imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
+        $imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = "${formatID}"
+        $imageProcess.Filters.Item(1).Properties.Item("Quality").Value = 5
+        $image = $imageProcess.Apply($image)
+        $bytes = $image.FileData.BinaryData
+        [System.Console]::OpenStandardOutput().Write($bytes, 0, $bytes.Length)
+      `),
+      o.timeout
+    );
+  } catch(err) {
+    if (typeof err === "string" && err.includes("At line:5") && err.includes("An unspecified error occurred during an attempted communication with the WIA device.")) {
+      throw Errors.connect;
+    } else if (typeof err === "string" && err.includes("At line:5") && err.includes("The WIA device is busy.")) {
+      throw Errors.busy;
+    } else {
+      throw err;
     }
-    $imageProcess = new-object -ComObject WIA.ImageProcess
-    foreach ($item in $device.Items) {
-      $image = $item.Transfer()
-    }
-    $imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
-    $imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = "${formatID}"
-    $imageProcess.Filters.Item(1).Properties.Item("Quality").Value = 5
-    $image = $imageProcess.Apply($image)
-    $bytes = $image.FileData.BinaryData
-    [System.Console]::OpenStandardOutput().Write($bytes, 0, $bytes.Length)
-  `;
-  return toBuffer(
-    run("powershell.exe", '-NoProfile', '-Command', `& {${ps}}`),
-    o.timeout
-  );
+  }
 };
 
 const linuxScan = (o: Options) =>
@@ -80,13 +104,24 @@ export const scan = (o: Options) => {
   }
 };
 
+const windowsList = async (timeout?: number) => {
+  const rawDevices = await toBuffer(powershell(`  
+    $ErrorActionPreference = "Stop"
+    $deviceManager = new-object -ComObject WIA.DeviceManager
+    $deviceManager.DeviceInfos | ForEach-Object -Process {$_.DeviceId}
+  `), timeout);
+  return rawDevices.toString().split("\r\n").filter(x => x);
+}
+
 const linuxList = async (timeout?: number) => {
   const rawDevices = await toBuffer(run("scanimage", "-f", "%d%n"), timeout);
   return rawDevices.toString().split("\n").filter(x => x);
 }
-
+  
 export const list = (timeout?: number) => {
   switch (process.platform) {
+    case 'win32':
+      return windowsList(timeout);
     case 'linux':
       return linuxList(timeout);
     default:
--- test.js
+++ test.js
@@ -1,14 +1,26 @@
-const { scan, list } = require("./lib/index");
+const { scan, list, Errors } = require("./lib/index");
 const { writeFileSync } = require("fs");
+const { assert } = require("console");
 
 const main = async () => {
   const devices = await list();
   console.log({ devices });
   try {
-    const buffer = await scan({ format: "bmp", dpi: 75 });
-    writeFileSync("file.bmp", buffer);
+    const buffer = await scan({ format: "bmp", dpi: 75, device: devices[0] });
+    writeFileSync("device1.bmp", buffer);
   } catch(e) {
     console.error(e);
+  }
+  try {
+    const buffer = await scan({ format: "bmp", dpi: 75 });
+    writeFileSync("deviceUndefined.bmp", buffer);
+  } catch(e) {
+    console.error(e);
+  }
+  try { // expect busy
+    await Promise.all([scan({ format: "bmp", dpi: 75 }), scan({ format: "bmp", dpi: 75 })]);
+  } catch(e) {
+    assert(e.code === Errors.busy.code)
   }
 };
 
